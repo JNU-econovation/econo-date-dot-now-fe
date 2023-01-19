149f2325c27707b19a88509d2ec0e37c887dbec7
j8won <joowoni99@gmail.com>
2023-01-20 03:13:42 +0900

    fix(login): html/css/js 수정

--- login/logIn.js
+++ login/logIn.js
@@ -1,121 +1,46 @@
 
-"use strict";
+const emailInput = document.querySelector("#email"),
+    pswordInput = document.querySelector("#password"),
+    loginBtn = document.querySelector(".login-button"),
+    errorMessage = document.querySelector(".error-message"),
+    form = document.querySelector(".login-form");
 
-const email = document.querySelector("#email1"), 
-    psword = document.querySelector("#passwordbox"),
-    loginBtn = document.querySelector(".logInButton"),
-    eM = document.querySelector(".errorMessage"),
-    form = document.querySelector("form");
+function errorEmail(){
+    errorMessage.innerText="이메일을 입력해 주세요"
+}
 
+function errorPassword(){
+    errorMessage.innerText="비밀번호를 입력해 주세요"
+}
 
-let check="";
-loginBtn.addEventListener("click",login);
+function login() {
 
-form.addEventListener("keyup",enterkey);
+    if (emailInput.value === "") {
+        errorEmail();
+    } else if (pswordInput.value === "") {
+        errorPassword();
+    } else {
+        errorMessage.innerText="";
+        console.log(emailInput.value, pswordInput.value);
 
-//폼에 이벤트 폼 submit,a 태그 를 eventpreveDefault 폼 하나로 다 할필요는 없음
-// 폼  
-//게시판 토스트 ui api 
-function enterkey(event) {
-	if (event.keyCode === 13) {
-        login1();
+        axios.post("", {email: emailInput.value, password: pswordInput.value})
     }
 }
-//d
 
- //엔터를 하려면 preventDefault()를 못 읽나 ?
-
-
-function errorMessage1(){
-    eM.innerText="이메일을 입력해 주세요"
-    
-}
-function errorMessage2(){
-    eM.innerText="비밀번호를 입력해 주세요"
-}
-
-function login(event){
+function handleFormEnter(event) {
     event.preventDefault();
-    const logInUserInfo = {
-        email : email.value,
-        psword : psword.value
-    };
-
-    if(logInUserInfo.email==check && logInUserInfo.psword==check||logInUserInfo.email==check && logInUserInfo.psword!=check){
-        errorMessage1();
-    }else if(logInUserInfo.email!=check && logInUserInfo.psword==check){
-        errorMessage2();
-    }else{
-        eM.innerText="";
-        /* 나중에 하기!!
-        fetch("//url//", {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-            },
-            body : JSON.stringify(logInUserInfo),
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                if (res.success){ //유효성 검사. 로그인 성공의 key값이 success라면 
-                    location.href = " "; // 로그인 성공 시 이동 링크(홈화면링크)
-                }else {
-                    //로그인 안됐을때. 빨간 글씨로 경고 문자 띄우기. (이메일 또는 비번 잘못입력했습니다)
-                }         
-            })
-            .catch((err)=> {
-                console.error(new Error("로그인 에러 발생"));
-            }); // 에러났을 때 ...
-        */
+    if (event.keyCode === 13) {
+        login();
     }
-    console.log(logInUserInfo);
+}
 
-
-
+function handleButtonClick() {
+    login();
 }
 
 
-
-function login1(event){
-    
-    const logInUserInfo = {
-        email : email.value,
-        psword : psword.value
-    };
-
-    if(logInUserInfo.email==check && logInUserInfo.psword==check||logInUserInfo.email==check && logInUserInfo.psword!=check){
-        errorMessage1();
-    }else if(logInUserInfo.email!=check && logInUserInfo.psword==check){
-        errorMessage2();
-    }else{
-        eM.innerText="";
-        /* 나중에 하기!!
-        fetch("//url//", {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-            },
-            body : JSON.stringify(logInUserInfo),
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                if (res.success){ //유효성 검사. 로그인 성공의 key값이 success라면 
-                    location.href = " "; // 로그인 성공 시 이동 링크(홈화면링크)
-                }else {
-                    //로그인 안됐을때. 빨간 글씨로 경고 문자 띄우기. (이메일 또는 비번 잘못입력했습니다)
-                }         
-            })
-            .catch((err)=> {
-                console.error(new Error("로그인 에러 발생"));
-            }); // 에러났을 때 ...
-        */
-    }
-    console.log(logInUserInfo);
-
-
-
-}
-
+loginBtn.addEventListener("click", handleButtonClick);
+form.addEventListener("submit", handleFormEnter);
 
 
 
